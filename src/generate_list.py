3db19d94c7790778b05faf7b047aa73d2287b49f
Thanawan <su.tanawn_st@tni.ac.th>
2017-02-03 03:02:40 +0000

    Commit 5 -generate_list v2 tested

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,7 +1,8 @@
 import random
 def generate_list():
     alist = [x for x in range (random.randint (-10, 10)) ]
-    assert alist
+    assert alist is None
+    assert(sum(alist)<0)
     return alist
     
 """
@@ -9,7 +10,8 @@ print a generated list
 """
 
 def printIt():
-    print( generate_list())
+    for i in range(1,10):
+        print( generate_list())
     
 def main():
     printIt()
